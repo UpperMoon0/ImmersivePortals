fbacaee773d572da9991aec88eb44730ece8e95e
Sunday Vo <60876500+UpperMoon0@users.noreply.github.com>
2026-09-14 19:50:43 +0700

    fix: restore q_misc_util NeoForge mod identity

--- src/main/java/qouteall/q_misc_util/MiscUtilModEntry.java
+++ src/main/java/qouteall/q_misc_util/MiscUtilModEntry.java
@@ -2,10 +2,12 @@ package qouteall.q_misc_util;
 
 import de.nick1st.q_misc_util.networking.Payloads;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 import qouteall.q_misc_util.dimension.DimensionIntId;
 
 import static qouteall.q_misc_util.MiscUtilModEntry.MOD_ID;
 
+@Mod(MOD_ID)
 public class MiscUtilModEntry {
     public static final String MOD_ID = "q_misc_util";
 
